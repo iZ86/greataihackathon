ea1645ffc69fa70266e68d6c5288dce25ffe41a7
iZ86 <iZ86LikesCS@gmail.com>
2025-09-20 19:05:02 +0800

    Add PreAuth to redirect users if not logged in

--- src/app/(routes)/chat/page.tsx
+++ src/app/(routes)/chat/page.tsx
@@ -3,13 +3,18 @@
 import ChatArea from "@/components/ChatArea";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import PreAuth from "../../../components/PreAuth";
 
 export default function Chat() {
   return (
+
     <div className="bg-background-light dark:bg-background-dark font-display text-gray-900 dark:text-white min-h-screen flex flex-col">
       <Navbar />
-      <Sidebar page="chat" />
-      <ChatArea />
+      <PreAuth>
+        <Sidebar page="chat" />
+        <ChatArea />
+
+      </PreAuth>
     </div>
   );
 }
