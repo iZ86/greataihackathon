0a4029cb22a49a75e95e933827242afc78a29438
iZ86 <iZ86LikesCS@gmail.com>
2025-09-20 19:04:06 +0800

    Add Amplify.configure here
    Root file calls this AuthProvider wrapper, and it is client side, so
    this is a good place to put Amplify.configure rather than root file
    which is server side

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,4 +1,8 @@
 'use client';
+import { Amplify } from 'aws-amplify';
+import outputs from "../../amplify_outputs.json";
+
+Amplify.configure(outputs);
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import React from 'react';
