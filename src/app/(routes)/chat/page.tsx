46c0d5726897b85db693799ed792a43ca231263d
skyfoojs <skyfoojs@gmail.com>
2025-09-20 15:29:33 +0800

    Add chat history button

--- src/app/(routes)/chat/page.tsx
+++ src/app/(routes)/chat/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ChatArea from "@/components/ChatArea";
+import ChatHistorySidebar from "@/components/ChatHistorySidebar";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
@@ -8,6 +9,7 @@ export default function Chat() {
   return (
     <div className="bg-background-light dark:bg-background-dark font-display text-gray-900 dark:text-white min-h-screen flex flex-col">
       <Navbar />
+      <ChatHistorySidebar />
       <Sidebar page="chat" />
       <ChatArea />
     </div>
