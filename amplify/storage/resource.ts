8c0031a0fe6b80d101adc3817394b861b9f8d3fa
skyfoojs <skyfoojs@gmail.com>
2025-09-21 10:25:58 +0800

    Extract data to env

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,10 +1,13 @@
 import { defineStorage } from "@aws-amplify/backend";
 
+const BUCKET_FOLDER: string = process.env.BUCKET_FOLDER as string;
+const BUCKET_NAME: string = process.env.BUCKET_NAME as string;
+
 export const storage = defineStorage(
   {
-    name: 'medical-query-bucket',
+    name: BUCKET_NAME,
     access: (allow) => ({
-      'medical-records/*': [
+      BUCKET_FOLDER: [
         allow.authenticated.to(['read', 'write', 'delete'])
       ],
     }),
