f8d596d0674a24a78390d842e4c4e99163d55960
skyfoojs <skyfoojs@gmail.com>
2025-09-21 20:43:36 +0800

    Add logo and description

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,11 @@
+import Image
+ from "next/image";
 export default function Navbar() {
   return (
     <header className="border-b border-white/10 px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
         <div className="flex items-center gap-2">
-          <svg
-            className="h-8 w-8 text-primary"
-            fill="none"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12C4 7.58172 7.58172 4 12 4ZM13 14V17H11V14H8V12H11V9H13V12H16V14H13Z"
-              fill="currentColor"
-            ></path>
-          </svg>
+          <Image src="/logo.png" width={40} height={40} alt="HealthAI Logo" />
           <h1 className="text-xl font-bold">HealthAI</h1>
         </div>
       </div>
